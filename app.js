58c8113b66a6ec598686aa87425b7963ec48df73
Nagyvee <nigelmafuta1@gmail.com>
2024-05-16 09:27:47 +0200

    quiote generator api

---
+++ app.js
@@ -0,0 +1,24 @@
+const app = require('express')()
+const { json } = require('express')
+const fs = require('fs')
+
+app.get('/', (req,res)=>{
+
+    // Reading the JSON File: Use Node.js’s built-in fs module to read the quotes.json file
+    fs.readFile('./qoutes/qoutes.js' ,'utf8', (err,data)=>{
+        if(err) console.log('There is an error:',err)
+        const jsonData = JSON.parse(data)
+        const dataLength = jsonData.length
+        const randomNumber = Math.floor(Math.random()*dataLength)+ 1
+
+        // Parsing the JSON Data: Once you’ve read the file, you’ll need to parse it from a string into a JavaScript object. && // Generating a Random Quote: Create a function that selects a random quote from the parsed JSON object.
+        const randomQuote = jsonData[randomNumber]
+
+        // Displaying the Quote: Print the random quote to the console, along with the author’s name.
+        console.log(`Author: ${randomQuote.author} \t\t Quote: ${randomQuote.quote}` )
+
+        res.send('Random quote logged to the console')
+    })
+})
+
+app.listen(5500)
---
+++ qoutes/qoutes.js
@@ -0,0 +1,12 @@
+[
+    { "quote": "The only thing we have to fear is fear itself.", "author": "Franklin D. Roosevelt" },
+    { "quote": "The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well.", "author": "Ralph Waldo Emerson" },
+    { "quote": "Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.", "author": "Martin Luther King Jr." },
+    { "quote": "With great power comes great responsibility.", "author": "Uncle Ben (from Spiderman)"}, 
+    { "quote": "A journey of a thousand miles begins with a single step.", "author": "Lao Tzu" },
+    { "quote": "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.", "author": "Helen Keller" },
+    { "quote": "You don't have to be great to start, but you have to start to be great.", "author": "Zig Ziglar" },
+    { "quote": "Never doubt that a small group of thoughtful, committed citizens can change the world; indeed, it's the only thing that ever has.", "author": "Margaret Mead" },
+    { "quote": "The mind is everything. What you think you become.", "author": "Buddha" },
+    { "quote": "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do. So throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails. Explore, Dream, Discover.", "author": "Mark Twain" }
+    ]
